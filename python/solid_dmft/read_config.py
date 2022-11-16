e8ffc995c2ae76d64b7063202c067a002f6bb8e5
Alexander Hampel <ahampel@flatironinstitute.org>
2022-11-16 14:43:27 -0500

    [test] add Hartree Solver test

--- python/solid_dmft/read_config.py
+++ python/solid_dmft/read_config.py
@@ -330,7 +330,7 @@ hartree parameters
 ================
 with_fock : bool, optional, default=False
         include Fock exchange terms in the self-energy
-force_real : bool, optional, default=False
+force_real : bool, optional, default=True
         force the self energy from Hartree fock to be real
 one_shot : bool, optional, default=False
         True if the calcualtion is just one shot and not self consistent. Default is False
@@ -761,7 +761,7 @@ PROPERTIES_PARAMS = {'general': {'seedname': {'used': True},
                                                  'default': False},
                                 'force_real': {'converter': BOOL_PARSER,
                                                  'used': lambda params: params['general']['solver_type'] in ['hartree'],
-                                                 'default': False},
+                                                 'default': True},
 
                                 #
                                 # ftps parameters
