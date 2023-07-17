e94e6ade0106695469bf6553f20a54cdd954c440
Alexander Hampel <ahampel@flatironinstitute.org>
2023-07-17 09:46:33 -0400

    [feat] add direct G_iw cthyb measurement

--- test/python/svo_cthyb_basic_tf/test.py
+++ test/python/svo_cthyb_basic_tf/test.py
@@ -2,7 +2,7 @@ import sys
 import shutil
 
 from triqs.gf import *
-from triqs.utility.comparison_tests import assert_block_gfs_are_close
+from triqs.utility.comparison_tests import assert_block_gfs_are_close, assert_arrays_are_close
 from h5 import HDFArchive
 import triqs.utility.mpi as mpi
 
@@ -16,4 +16,15 @@ mpi.barrier()
 
 solid.main([None, 'dmft_config.ini'])
 
+with HDFArchive('out/inp.h5','r') as ar:
+    G_iw = ar['DMFT_results/last_iter/Gimp_freq_0']
+    G_iw_direct = ar['DMFT_results/last_iter/Gimp_freq_direct_0']
 
+
+# compare direct measured G_iw
+iw0 = len(G_iw.mesh)//2
+n_iw = len(G_iw_direct.mesh) // 2
+
+# works only if statistics are improved to higher precision
+for block, gf in G_iw_direct:
+    assert_arrays_are_close(G_iw[block].data[iw0-n_iw:iw0+n_iw, :, :], G_iw_direct[block].data, precision=5e-2)
