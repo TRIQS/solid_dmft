1881635b79df28cf7741944511129f96dc722d09
the-hampel <mail@alexander-hampel.de>
2025-06-27 15:14:00 +0200

    [fix] hartree interface postprocessing stored unsymmetrized Gfimp

--- python/solid_dmft/dmft_tools/solver.py
+++ python/solid_dmft/dmft_tools/solver.py
@@ -1252,8 +1252,8 @@ class SolverStructure:
         # get everything from solver
         self.G0_freq << self.triqs_solver.G0_iw
         self.G_freq_unsym << self.triqs_solver.G_iw
+        self.G_freq << self.triqs_solver.G_iw
         self.sum_k.symm_deg_gf(self.G_freq, ish=self.icrsh)
-        self.G_freq << self.G_freq
         for bl, gf in self.Sigma_freq:
             self.Sigma_freq[bl] << self.triqs_solver.Sigma_HF[bl]
             self.Sigma_Refreq[bl] << self.triqs_solver.Sigma_HF[bl]
