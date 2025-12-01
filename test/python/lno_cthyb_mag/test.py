2020a55f539eebb0e74f369cfcb8c67db66370dc
the-hampel <mail@alexander-hampel.de>
2025-12-01 14:24:58 +0100

    [test] add missing test for afm order and cthyb for better test coverage

---
+++ test/python/lno_cthyb_mag/test.py
@@ -0,0 +1,27 @@
+import sys
+import shutil
+
+from triqs.gf import *
+from triqs.utility.comparison_tests import assert_block_gfs_are_close
+from h5 import HDFArchive
+import triqs.utility.mpi as mpi
+
+import solid_dmft.main as solid
+
+
+if mpi.is_master_node():
+    shutil.rmtree('out', ignore_errors=True)
+
+mpi.barrier()
+
+solid.main([None, 'dmft_config.toml'])
+
+mpi.barrier()
+
+if mpi.is_master_node():
+    out = 'out/inp.h5'
+    ref = 'ref.h5'
+
+    out =  HDFArchive(out, 'r')['DMFT_results']['last_iter']
+    ref =  HDFArchive(ref, 'r')['DMFT_results']['last_iter']
+
